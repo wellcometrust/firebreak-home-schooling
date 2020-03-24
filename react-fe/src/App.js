0d1712c3685045d4989a382962fa0285aea850fe
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-03-24 10:09:44 +0000

    Added sass, removed unwanted styles and default markup

--- react-fe/src/App.js
+++ react-fe/src/App.js
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react';
-import logo from './logo.svg';
 import AdminContext from './components/AdminContext/AdminContext';
 
-import './App.css';
+import './assets/app.scss';
 
 function App() {
   const { isAdminActive, toggleAdmin } = useContext(AdminContext);
@@ -15,18 +14,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button type="button" onClick={handleClick}>{isAdminActive ? 'deactivate admin': 'activate admin'}</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
