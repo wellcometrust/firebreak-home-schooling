7cea3df3218d5fcba300baffdaa371186536d376
Teri Forey <teri.forey@gmail.com>
2020-03-24 15:06:36 +0000

    Changed table components to functions

--- react-fe/src/components/Table/table.js
+++ react-fe/src/components/Table/table.js
@@ -1,56 +1,48 @@
 import React from 'react';
 import './table.css';
 
-
-class Item extends React.Component {
-
-    render() {
-
-        this.perchild = this.props.value.data.map((child, key) => 
-            (
-                <div key={key} className="tableItem">
-                    <input type="checkbox" checked={child} onChange={() => this.props.onChange(key)}/>
-                </div>
-            )
-        );
-
-        return (
-            <div className="tableRow">
-                <div className="tableItem">{this.props.value.title}</div>
-                {this.perchild}
+function Item(props) {
+    const perchild = props.value.data.map((child, key) => 
+        (
+            <div key={key} className="tableItem">
+                <input type="checkbox" checked={child} onChange={() => props.onChange(key)}/>
             </div>
         )
-    }
+    );
+
+    return (
+        <div className="tableRow">
+            <div className="tableItem">{props.value.title}</div>
+            {perchild}
+        </div>
+    )
 }
 
 
-class Table extends React.Component {
+function Table(props) {
 
-    render() {
+    const cols = props.columns.map((col, i) => 
+        <div className="tableItem" key={i}>{col.name}</div>
+    );
 
-        this.cols = this.props.columns.map((col, i) => 
-            <div className="tableItem" key={i}>{col.name}</div>
-        );
+    const rows = props.rows.map((row, i) =>
+        <Item key={i} value={row} onChange={(k) => props.onChange(i, k)}/>
+    );
 
-        this.rows = this.props.rows.map((row, i) =>
-            <Item key={i} value={row} onChange={(k) => this.props.onChange(i, k)}/>
-        );
-
-        return (
-            <div className="table">
-                <div className="tableHeader">
-                    <div className="tableRow">
-                        <div className="tableItem">Expectation</div>
-                        {this.cols}
-                    </div>
-                </div>
-                <div className="tableBody">
-                    {this.rows}
+    return (
+        <div className="table">
+            <div className="tableHeader">
+                <div className="tableRow">
+                    <div className="tableItem">Expectation</div>
+                    {cols}
                 </div>
             </div>
-        )
+            <div className="tableBody">
+                {rows}
+            </div>
+        </div>
+    )
         
-    }
 }
 
 export default Table;
